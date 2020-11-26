1d0f7b884fde947eb204c85415ae5fcd60564da7
Rithvik Nishad <rithvikn2001@gmail.com>
2020-11-26 10:30:48 +0530

    updated dependencies

--- lib/service.dart
+++ lib/service.dart
@@ -1,9 +1,8 @@
-import 'package:bsi_dart/bsi_dart.dart';
+// import 'package:bsi_dart/bsi_dart.dart';
 
-class VirtualHardwareService extends Service {
-  String get id
-  
-  @override
-  ServiceReference get reference =>
-      Services.Hardwares.child('virtual-$hashCode');
-}
+// class VirtualHardwareService extends Service {
+//   String get id => '$hashCode';
+
+//   @override
+//   ServiceReference get reference => Services.Hardwares.child('virtualhw-$id');
+// }
